6a870c00840550300fcf7ead008e1e4a4d880d95
Crowdbotics <engineering@crowdbotics.com>
2023-07-31 18:49:59 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "delicate-limit-dev-105866",
+  url: "https://delicate-limit-dev-105866.botics.co",
+  api: "https://delicate-limit-dev-105866.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
